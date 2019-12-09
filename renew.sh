4997a7071a647bde523f21a289d4b6a620e7a8d3
Hajnal Mate <hajnalmt@gmail.com>
2019-12-09 18:24:11 +0100

    Added some flags...

--- renew.sh
+++ renew.sh
@@ -1,2 +1,2 @@
-certbot renew --dry-run --webroot --noninteractive --agree-tos --email $EMAIL --webroot-path=/usr/share/nginx/html --standalone-supported-challenges tls-sni-01
+certbot renew --dry-run --webroot --noninteractive --agree-tos --email $EMAIL --webroot-path=/usr/share/nginx/html --preferred-challenges html-01
 /usr/sbin/service nginx reload
